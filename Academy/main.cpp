94eb77a61e5068f64dbf4d6b9b927bb4b25996f5
Булат <4bxlia4@gmail.com>
2025-11-11 16:16:19 +0500

    .

--- Academy/main.cpp
+++ Academy/main.cpp
@@ -1,53 +1,83 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
-#include <time.h>
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<time.h>
+#include <sstream>
+#include <typeinfo>
 using std::cin;
 using std::cout;
 using std::endl;
 
 #define tab "\t"
-#define delimiter "\n-----------------------------------------\n"
+#define delimiter "\n-------------------------------------\n"
 
+//Default constructor - это конструктор, который может быть вызван без параметров.
 #define HUMAN_TAKE_PARAMETERS const std::string& last_name, const std::string& first_name, const std::string& birth_date
 #define HUMAN_GIVE_PARAMETERS last_name, first_name, birth_date
+//Директива - это указание компилятору на выполнение некоторых действий.
+//Директива #define создает МАКРООПРЕДЕЛЕНИЕ (МАКРОС).
+//У макроса есть ИМЯ и значение, и везде, где компилятор видет ИМЯ МАКРОСА он вставляет значение макроса,
+//все это происходит во время компиляции.
+//Имя макроса можно считать псевдонимом для фрагмента кода.
+
+//Директива #define показывает компилятору что заменить и чем заменить.
+//В нашем случае 'HUMAN_TAKE_PARAMETERS' заменяется на реальный список принимаемых параметров,
+//а 'HUMAN_GIVE_PARAMETERS' - на список передаваемых параметров.
+//Здесь 'HUMAN_GIVE_PARAMETERS' - это имя макроса
+//'last_name, first_name, birth_date' - значение макроса.
+//#ifdef MACROS ... #endif
 class Human
 {
-	const std::string LAST_NAME;
-	const std::string FIRST_NAME;
-	const tm BIRTH_DATE;
+	static const int TYPE_WIDTH = 9;
+	static const int LAST_NAME_WIDTH = 12;
+	static const int FIRST_NAME_WIDTH = 12;
+	static const int AGE_WIDTH = 5;
+	static const int TIMESTAMP_WIDTH = 11;
 
+	std::string last_name;
+	std::string first_name;
+	tm birth_date;
 public:
-	const std::string& get_last_name() const
+	const std::string& get_last_name()const
 	{
-		return LAST_NAME;
+		return last_name;
 	}
-	const std::string& get_first_name() const
+	const std::string& get_first_name()const
 	{
-		return FIRST_NAME;
+		return first_name;
 	}
-	tm get_birth_date() const
+	tm get_birth_date()const
 	{
-		return BIRTH_DATE;
+		return birth_date;
 	}
-	int get_age() const
+	time_t get_birth_date_timestamp()const
 	{
-		//1) Получаем текущее время в формате Timestamp
+		tm birth_date = this->birth_date;
+		return mktime(&birth_date);
+	}
+	int get_age()const
+	{
+		//1) Получаем текущее время в формате Timestamp:
 		time_t t_today = time(NULL);
 		tm tm_today = *localtime(&t_today);
-		//2) Преобразуем дату рождения в формате Timestamp
-		tm birth_date = BIRTH_DATE;
+		//2) Преобразуем дату рождения в формат Timestamp:
+		tm birth_date = this->birth_date;
 		time_t t_birth_date = mktime(&birth_date);
 
-		//3) Находим разницу во времени
+		//3) Находим разницу во времени:
 		time_t difference = t_today - t_birth_date;
 
-		//4) Преобразуем разницу во времени в формат 'tm'
+		//4) Преобразуем разницу во времени в формат 'tm':
 		tm age = *localtime(&difference);
 		age.tm_year -= 70;
 		return age.tm_year;
 	}
 	tm parse_date(const std::string& date)
 	{
+		//yyyy-MM-dd
+		//yyyy.MM.dd
+		//yyyy/MM/dd
 		char sz_date[32] = {};
 		strcpy(sz_date, date.c_str());
 		int date_parts[3] = {};
@@ -55,8 +85,8 @@ public:
 		const char delimiters[] = "-./";
 		for (char* pch = strtok(sz_date, delimiters); pch; pch = strtok(NULL, delimiters))
 			date_parts[n++] = atoi(pch);
-		date_parts[0] -= 1900; //структура 'tm' хранит дфту с 1 января 1900 года
-		date_parts[1]--; //месяца нумеруются с нуля
+		date_parts[0] -= 1900;	//структура 'tm' хранит дату с 1 января 1900 года.
+		date_parts[1]--;		//месяца нумеруются с нуля
 
 		tm tm_birth_date = {};
 		tm_birth_date.tm_year = date_parts[0];
@@ -65,22 +95,74 @@ public:
 		return tm_birth_date;
 	}
 	Human(HUMAN_TAKE_PARAMETERS) :
-		LAST_NAME(last_name), FIRST_NAME(first_name), BIRTH_DATE(parse_date(birth_date))
+		last_name(last_name), first_name(first_name), birth_date(parse_date(birth_date))
 	{
-		cout << "HConsructor:\t" << this << endl;
+		cout << "HConstructor:\t" << this << endl;
 	}
 	~Human()
 	{
-		cout << "HDesructor:\t" << this << endl;
+		cout << "HDestructor:\t" << this << endl;
 	}
-	virtual void info()const
+	virtual std::ostream& info(std::ostream& os)const
 	{
-		cout << LAST_NAME << " " << FIRST_NAME << " " << get_age() << endl;
+		//return os << LAST_NAME << " " << FIRST_NAME << " " << get_age();
+		os.width(TYPE_WIDTH);
+		os << std::left;
+		os << std::string(typeid(*this).name() + 6) + ":";
+		os.width(LAST_NAME_WIDTH);
+		os << last_name;
+		os.width(FIRST_NAME_WIDTH);
+		os << first_name;
+		os.width(AGE_WIDTH);
+		os << get_age();
+		return os;
 	}
-};
+	virtual std::ofstream& write(std::ofstream& os)const
+	{
+		//return os << LAST_NAME << " " << FIRST_NAME << " " << get_age();
+		os.width(TYPE_WIDTH);
+		os << std::left;
+		os << std::string(typeid(*this).name() + 6) + ":";
+		os.width(LAST_NAME_WIDTH);
+		os << last_name;
+		os.width(FIRST_NAME_WIDTH);
+		os << first_name;
+		os.width(TIMESTAMP_WIDTH);
+		os << get_birth_date_timestamp();
+		return os;
+	}
 
-class AcademyMember : public Human
+
+	/*
+	-----------------------------
+	__vfptr - Virtual Functions Pointers (Таблица указателей на виртуальные функции)
+	virtual type name(parameters) modifiers
+	{
+		......;
+		......;
+		......;
+	}
+	-----------------------------
+	*/
+};
+std::ostream& operator<<(std::ostream& os, const Human& obj)
 {
+	return obj.info(os);
+	/*return os
+		<< obj.get_last_name()
+		<< " " << obj.get_first_name()
+		<< " " << obj.get_age();*/
+}
+std::ofstream& operator<<(std::ofstream& ofs, const Human& obj)
+{
+	return obj.write(ofs);
+}
+
+#define ACADEMY_MEMBER_TAKE_PARAMETERS const std::string& speciality
+#define ACADEMY_MEMBER_GIVE_PARAMETERS speciality
+class AcademyMember :public Human
+{
+	const int SPECIALITY_WIDTH = 40;
 	std::string speciality;
 public:
 	const std::string get_speciality()const
@@ -91,12 +173,7 @@ public:
 	{
 		this->speciality = speciality;
 	}
-
-#define ACADEMY_MEMBER_TAKE_PARAMETERS const std::string& speciality
-#define ACADEMY_MEMBER_GIVE_PARAMETERS speciality
-	AcademyMember(HUMAN_TAKE_PARAMETERS,
-		ACADEMY_MEMBER_TAKE_PARAMETERS) :
-		Human(HUMAN_GIVE_PARAMETERS)
+	AcademyMember(HUMAN_TAKE_PARAMETERS, ACADEMY_MEMBER_TAKE_PARAMETERS) :Human(HUMAN_GIVE_PARAMETERS)
 	{
 		set_speciality(speciality);
 		cout << "AMConstructor:\t" << this << endl;
@@ -105,55 +182,64 @@ public:
 	{
 		cout << "AMDestructor:\t" << this << endl;
 	}
-	void info()const override
+	std::ostream& info(std::ostream& os)const override
 	{
-		Human::info(); //Вызываем метод info() для класса 'Human'
-		cout << speciality << endl;
+		//return Human::info(os) << " " << speciality;
+		Human::info(os);
+		os.width(SPECIALITY_WIDTH);
+		os << speciality;
+		return os;
+		//Human::info(os);	//Вызываем метод info() для класса 'Human'.
+		//cout << speciality << endl;
 	}
+	std::ofstream& write(std::ofstream& os)const override
+	{
+		Human::write(os);
+		os.width(SPECIALITY_WIDTH);
+		os << speciality;
+		return os;
+	}
+
 };
 
-#define STUDENT_TAKE_PARAMETERS  const std::string& group, double rating, double attendance
+#define STUDENT_TAKE_PARAMETERS const std::string& group, double rating, double attendance
 #define STUDENT_GIVE_PARAMETERS group, rating, attendance
-class Student : public AcademyMember
+class Student :public AcademyMember
 {
+	static const int GROUP_WIDTH = 8;
+	static const int RATING_WIDTH = 8;
+	static const int ATTENDANCE_WIDTH = 8;
 	std::string group;
 	double rating;
 	double attendance;
-
 public:
-	const std::string& get_group() const
+	const std::string& get_group()const
 	{
 		return group;
 	}
-
+	double get_rating()const
+	{
+		return rating;
+	}
+	double get_attendance()const
+	{
+		return attendance;
+	}
 	void set_group(const std::string& group)
 	{
 		this->group = group;
 	}
-
-	double get_rating() const
-	{
-		return rating;
-	}
-
 	void set_rating(double rating)
 	{
-		if (rating >= 0 && rating <= 100)
-			this->rating = rating;
+		this->rating = rating;
 	}
-
-	double get_attendance() const
-	{
-		return attendance;
-	}
-
 	void set_attendance(double attendance)
 	{
-		if (attendance >= 0 && attendance <= 100)
-			this->attendance = attendance;
+		this->attendance = attendance;
 	}
 
-	Student(HUMAN_TAKE_PARAMETERS, ACADEMY_MEMBER_TAKE_PARAMETERS, const std::string& group, double rating, double attendance) :
+	//				Constructors:
+	Student(HUMAN_TAKE_PARAMETERS, ACADEMY_MEMBER_TAKE_PARAMETERS, STUDENT_TAKE_PARAMETERS) :
 		AcademyMember(HUMAN_GIVE_PARAMETERS, ACADEMY_MEMBER_GIVE_PARAMETERS)
 	{
 		set_group(group);
@@ -161,86 +247,276 @@ public:
 		set_attendance(attendance);
 		cout << "SConstructor:\t" << this << endl;
 	}
-
 	~Student()
 	{
 		cout << "SDestructor:\t" << this << endl;
 	}
 
-	void info() const override
+	//			/Methods:
+	std::ostream& info(std::ostream& os)const override
 	{
-		AcademyMember::info();
-		cout << "Group: " << group << ", Rating: " << rating << "%, Attendance: " << attendance << "%" << endl;
+		//return AcademyMember::info(os) << " " << group << " " << rating << " " << attendance;
+		AcademyMember::info(os);
+		os.width(GROUP_WIDTH);
+		os << group;
+		os.width(RATING_WIDTH);
+		os << rating;
+		os.width(ATTENDANCE_WIDTH);
+		os << attendance;
+		return os;
+		//AcademyMember::info();
+		//cout << group << " " << rating << " " << attendance << endl;
 	}
+	std::ofstream& write(std::ofstream& os)const override
+	{
+		AcademyMember::write(os);
+		os.width(GROUP_WIDTH);
+		os << group;
+		os.width(RATING_WIDTH);
+		os << rating;
+		os.width(ATTENDANCE_WIDTH);
+		os << attendance;
+		return os;
+	}
+
 };
 
 #define TEACHER_TAKE_PARAMETERS int experience
 #define TEACHER_GIVE_PARAMETERS experience
-class Teacher : public AcademyMember
+class Teacher :public AcademyMember
 {
 	int experience;
-
 public:
-	int get_experience() const
+	int get_experience()const
 	{
 		return experience;
 	}
-
 	void set_experience(int experience)
 	{
-		if (experience >= 0)
-			this->experience = experience;
+		this->experience = experience;
 	}
-
-	Teacher(HUMAN_TAKE_PARAMETERS, ACADEMY_MEMBER_TAKE_PARAMETERS,
-		TEACHER_TAKE_PARAMETERS) :
+	//				Cnstructors:
+	Teacher(HUMAN_TAKE_PARAMETERS, ACADEMY_MEMBER_TAKE_PARAMETERS, TEACHER_TAKE_PARAMETERS) :
 		AcademyMember(HUMAN_GIVE_PARAMETERS, ACADEMY_MEMBER_GIVE_PARAMETERS)
 	{
 		set_experience(experience);
 		cout << "TConstructor:\t" << this << endl;
 	}
-
 	~Teacher()
 	{
 		cout << "TDestructor:\t" << this << endl;
 	}
-
-	void info() const override
+	//				Methods:
+	std::ostream& info(std::ostream& os)const
 	{
-		AcademyMember::info();
-		cout << "Experience: " << experience << " years" << endl;
+		return AcademyMember::info(os) << experience;
+		//AcademyMember::info();
+		//cout << experience << endl;
+	}
+	std::ofstream& write(std::ofstream& os)const
+	{
+		AcademyMember::write(os) << experience;
+		return os;
+		//AcademyMember::info();
+		//cout << experience << endl;
+	}
+
+};
+
+#define GRADUATE_TAKE_PARAMETERS const std::string& subject
+#define GRADUATE_GIVE_PARAMETERS subject
+class Graduate :public Student
+{
+	std::string subject;
+public:
+	const std::string& get_subject()const
+	{
+		return subject;
+	}
+	void set_subject(const std::string& subject)
+	{
+		this->subject = subject;
+	}
+	Graduate
+	(
+		HUMAN_TAKE_PARAMETERS,
+		ACADEMY_MEMBER_TAKE_PARAMETERS,
+		STUDENT_TAKE_PARAMETERS,
+		GRADUATE_TAKE_PARAMETERS
+	) :Student(HUMAN_GIVE_PARAMETERS, ACADEMY_MEMBER_GIVE_PARAMETERS, STUDENT_GIVE_PARAMETERS)
+	{
+		set_subject(subject);
+		cout << "GConstructor:\t" << this << endl;
+	}
+	~Graduate()
+	{
+		cout << "GDestructor:\t" << this << endl;
+	}
+	std::ostream& info(std::ostream& os)const override
+	{
+		return Student::info(os) << " " << subject;
+		//Student::info();
+		//cout << subject << endl;
 	}
 };
+
+void Print(Human* group[], const int n);
+void Save(Human* group[], const int n, const std::string& filename);
+Human** LoadFromFile(const std::string& filename, int& count);
+
+//#define INHERITANCE
 
 void main()
 {
 	setlocale(LC_ALL, "");
-#ifdef  INHERITANCE
-	cout << "|| Academy ||" << endl;
-	Human human("Тупенко ", "Василий", "2009.09.2");
+
+#ifdef INHERITANCE
+	cout << "Academy" << endl;
+	Human human("Тупенко", "Василий", "2009.09.02");
 	human.info();
+
 	AcademyMember am("Тупенко", "Василий", "2000.10.24", "Флуктуации пространства в вакууме");
 	am.info();
 
 	Student student("Чухарев", "Матвей", "2009.09.02", "Разработка программного обеспечения", "P_421", 100, 100);
 	student.info();
 
-	Teacher teacher("Энштейн", "Альберт", "1979.03.14", "Астрономия", 20);
+	Teacher teacher("Einstein", "Albert", "1979.03.14", "Astronomy", 20);
 	teacher.info();
-#endif //  INHERITANCE
+#endif // INHERITANCE
+	//Generalisation - обобщение;
+	//Upcast - это приведение дочернего объекта к базовому типу;
 	Human* group[] =
 	{
 		new Student("Чухарев", "Матвей", "2009.09.02", "Разработка программного обеспечения", "P_421", 100, 100),
-		new Teacher("Энштейн", "Альберт", "1979.03.14", "Астрономия", 20),
-		new Student("Гусев", "Савелий", "2010.08.29", "Разработка программного обеспечения", "P_421", 98, 98),
-		new Teacher("Олег", "Анатольевич", "1985.01.16", "Разработка программного обеспечения", 16),
-		new Student("Львов", "Константин", "2009.09.21", "Разработка программного обеспечения", "P_421", 100, 98),
+		new Teacher("Einstein", "Albert", "1979.03.14", "Astronomy", 20),
+		new Student("Гусев", "Савелий", "2010.08.29", "Разработка программного обеспечения", "P_421",98,98),
+		new Teacher("Олег", "Анатольевич","1985.01.16", "Разработка программного обеспечения", 16),
+		new Graduate("Львов", "Константин", "2009.09.21", "Разработка программного обеспечения", "P_421", 100, 98, "Разработка системы доставки пиццы")
 	};
+
+	Print(group, sizeof(group) / sizeof(group[0]));
+	Save(group, sizeof(group) / sizeof(group[0]), "group.txt");
+
+	int loaded_count = 0;
+	Human** loaded_group = LoadFromFile("group.txt", loaded_count);
+
+	if (loaded_group && loaded_count > 0)
+	{
+		Print(loaded_group, loaded_count);
+
+		for (int i = 0; i < loaded_count; i++)
+		{
+			delete loaded_group[i];
+		}
+		delete[] loaded_group;
+	}
+
+	for (int i = 0; i < sizeof(group) / sizeof(group[0]); i++)
+	{
+		delete group[i];
+	}
+}
+void Print(Human* group[], const int n)
+{
 	cout << sizeof(group) << endl;
 	cout << delimiter << endl;
-	for (int i = 0; i < sizeof(group)/sizeof(group[0]); i++)
+	for (int i = 0; i < n; i++)
 	{
-		group[i]->info();
+		//group[i]->info();
+		//<< - оператор перенаправления в поток
+		cout << *group[i] << endl;
 		cout << delimiter << endl;
 	}
+}
+void Save(Human* group[], const int n, const std::string& filename)
+{
+	std::ofstream fout(filename);
+	for (int i = 0; i < n; i++)
+	{
+		fout << typeid(*group[i]).name() + 6 << ": " << *group[i] << endl;
+	}
+	fout.close();
+	std::string cmd = "notepad ";
+	cmd += filename;
+	system(cmd.c_str());
+}
+
+Human** LoadFromFile(const std::string& filename, int& count)
+{
+	std::ifstream fin(filename);
+	if (!fin.is_open())
+	{
+		cout << filename << endl;
+		count = 0;
+		return nullptr;
+	}
+
+	count = 0;
+	std::string line;
+	while (std::getline(fin, line))
+	{
+		if (!line.empty()) count++;
+	}
+
+	if (count == 0)
+	{
+		fin.close();
+		return nullptr;
+	}
+
+	fin.clear();
+	fin.seekg(0);
+
+	Human** group = new Human * [count];
+
+	for (int i = 0; i < count; i++)
+	{
+		std::string type, last_name, first_name, speciality, group_name, subject;
+		time_t timestamp;
+		double rating, attendance;
+		int experience;
+
+		fin >> type;
+
+		if (type == "Student:")
+		{
+			fin >> last_name >> first_name >> timestamp >> speciality >> group_name >> rating >> attendance;
+			time_t temp_time = timestamp;
+			tm* temp_tm = localtime(&temp_time);
+			char date_str[11];
+			strftime(date_str, sizeof(date_str), "%Y.%m.%d", temp_tm);
+
+			group[i] = new Student(last_name, first_name, date_str, speciality, group_name, rating, attendance);
+		}
+		else if (type == "Teacher:")
+		{
+			fin >> last_name >> first_name >> timestamp >> speciality >> experience;
+			time_t temp_time = timestamp;
+			tm* temp_tm = localtime(&temp_time);
+			char date_str[11];
+			strftime(date_str, sizeof(date_str), "%Y.%m.%d", temp_tm);
+
+			group[i] = new Teacher(last_name, first_name, date_str, speciality, experience);
+		}
+		else if (type == "Graduate:")
+		{
+			time_t temp_time = timestamp;
+			tm* temp_tm = localtime(&temp_time);
+			char date_str[11];
+			strftime(date_str, sizeof(date_str), "%Y.%m.%d", temp_tm);
+
+			group[i] = new Graduate(last_name, first_name, date_str, speciality, group_name, rating, attendance, subject);
+		}
+		else
+		{
+			std::getline(fin, line);
+			i--;
+			count--;
+			continue;
+		}
+	}
+
+	fin.close();
+	return group;
 }
